af5801a74f8b2f25454050b9b620f780af7a8c05
Christopher Mason <cmason@counsyl.com>
2015-12-31 18:01:42 -0800

    Remove comment.

--- civet/compilers/es6.py
+++ civet/compilers/es6.py
@@ -12,7 +12,7 @@ es6_extension = getattr(settings, 'CIVET_ES6_EXTENSION', '.js')
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc:  # Python >2.5
+    except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
